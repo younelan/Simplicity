c714bc5905c006697314651a6bc03da1a86525de
Youness El Andaloussi <youness@elandaloussi.net>
2024-06-24 23:42:36 +0100

    group demo for users in case someone to remind it's a demo

--- demo/SimpleAuth/users.php
+++ demo/SimpleAuth/users.php
@@ -1,8 +1,8 @@
 <?php 
   /* simple list of users to be used for login demo, you probably want a database instead */
   $pref_users= [
-	  'admin'=>[ 'first' => 'Simple Demo' , 'last' => 'User' , 'password' => 'admin', 'group'=>'admin' ],
-	  'user'=>[ 'first' => 'Simple Demo' , 'last' => 'User' , 'password' => 'user', 'group'=>'users' ],
+	  'admin'=>[ 'first' => 'Simple Demo' , 'last' => 'User' , 'password' => 'admin', 'group'=>'demo-admin' ],
+	  'user'=>[ 'first' => 'Simple Demo' , 'last' => 'User' , 'password' => 'user', 'group'=>'demo-users' ],
   ];
 
   /* for demo purposes encrypt the passwords, you would not want to store them in plain text */
