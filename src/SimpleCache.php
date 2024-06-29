aed93d6e25cfddec08329c2c09217d0d15dfa2c9
Youness El Andaloussi <youness@elandaloussi.net>
2024-06-29 02:29:11 +0100

    Simplicity Http Request and SimpleCache

---
+++ src/SimpleCache.php
@@ -0,0 +1,38 @@
+<?php 
+    namespace Opensitez\Simplicity;
+    class SimpleCache {
+        private $options=[];
+        function setOptions($options) {
+            $this->options = $options;
+        }
+        function getFileName($url) {
+            $cache_dir=$this->options['cache-dir']?? (__DIR__ . "/cache");
+            $urlmd5=md5($url);
+            $cache_file="$cache_dir/$urlmd5.txt"; 
+
+            return $cache_file;
+        }
+        function isCacheCurrent($url,$timeout) {
+            $cache_file = $this->getFileName($url);
+            $seconds = $this->options['seconds']??3;
+            $timeout = $this->options['timeout']??300;
+
+            if ( file_exists("$cache_file")) { 
+                $timediff=time() - filemtime ( "$cache_file" );  
+                if($timediff > $seconds) {
+                    return false;
+                } else {
+                    return true;
+                }
+            }            
+        }
+        function put($url,$page_contents) {
+            $cache_file = $this->getFileName($url);
+            file_put_contents($cache_file,$page_contents);            
+        }
+        function get($url) {
+            $cache_file = $this->getFileName($url);
+            $page_contents=file_get_contents($cache_file);
+            return $page_contents;
+        }
+    }    
