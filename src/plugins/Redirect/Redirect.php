c54aa48179b86aeb3a2ef2817678d7b53451063d
Youness El Andaloussi <youness@elandaloussi.net>
2025-06-18 22:36:44 +0100

    Redirect Plugin

---
+++ src/plugins/Redirect/Redirect.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Opensitez\Simplicity\Plugins;
+
+use Opensitez\Simplicity\MSG;
+
+class Redirect extends \Opensitez\Simplicity\Plugin
+{
+    public $name = "Redirect";
+    public $description = "URL Redirection/ Shortener Plugin";
+    
+    function on_event($event)
+    {
+        switch ($event['type']) {
+            case MSG::PluginLoad:
+                // Register this plugin as a route type handler for redirects
+                $this->plugins->register('routetype', 'redirect');
+                break;
+        }
+        return parent::on_event($event);
+    }
+    
+    function get_menus($app = [])
+    {
+        $menus = [
+            "content" => [
+                "text" => "Content",
+                "image" => "genimgwebsite1.png",
+                "children" => [
+                    "redirects" => ["plugin" => "redirects", "page" => "default", "text" => "Redirects", "category" => "all"],
+                ]
+            ],
+
+        ];
+        return $menus;
+    }
+    public function render_page($app)
+    {
+        //print_r($app);exit;
+        $url = $app['url'] ?? "/";
+        $code = $app['code'] ?? 301;
+        header("Location: $url", true, $code);
+        print("Location: $url " . $code);
+        exit();
+    }
+    public function on_render_page($app)
+    {
+        $output = $this->render_page($app);
+        return $output;
+    }
+    public function get_html()
+    {
+        return $this->render_page();
+    }
+}
