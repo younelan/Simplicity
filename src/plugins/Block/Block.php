98b23e8b885e83ecf8afec8e9c560905fb5f7b6f
Youness El Andaloussi <youness@elandaloussi.net>
2025-06-22 13:30:59 +0100

    blocks now can include as content source

--- src/plugins/Block/Block.php
+++ src/plugins/Block/Block.php
@@ -7,27 +7,30 @@
         public $name="Block";
         public $block_options=[];
         public $block_name="";
-        public $block_type="";
+        public $block_type="block";
+        public $content_type = "text"; // Default content type for blocks
 
         public $default = [
             'encoding' => 'utf-8',
             'content-type' => 'html'
         ]; // Default options for blocks
-        public $content_type="html"; // Default content type for rendering
         public $text_block=false;
         public $description="Implements a basic block";
         public function set_block_options($options) {
             $this->block_options = $options;
             $this->block_name = $options['name']??'undefined';
-            $this->block_type = $options['content-type']??"text";
-            //print "<div>new block of {$this->block_name} of type {$this->block_type}</div>";
+            $this->block_type = $options['type'] ?? 'block';
+            $this->content_type = $options['content-type']??"text";
         }
         function on_event($event)
         {
             switch ($event['type']) {
                 case MSG::PluginLoad:
                     // Register this plugin as a route type handler for redirects
+                    $this->plugins->register_type('sectiontype', 'block');
+                    $this->plugins->register_type('sectiontype', 'include');
                     $this->plugins->register_type('routetype', 'block');
+                    $this->plugins->register_type('routetype', 'include');
                     break;
             }
             return parent::on_event($event);
@@ -62,6 +65,8 @@
             $i18n = $this->plugins->get_plugin('i18n');
             $section=$this->block_options;
             $this->content_type = $app['content-type'] ?? 'html';
+            $this->block_type = $app['type'] ?? 'block';
+            $fname = $app['file'] ?? '';
             $blocklink=$section['link']??"";
             $blockclass=$section['class']??"block block-$this->block_name";
             $retval = "";
@@ -77,6 +82,11 @@
             }
             /* render the content */
             // First try to get a registered block type plugin
+            $datafolder = $this->config_object->get('paths')['datafolder'] ?? '';
+           
+            $content = file_get_contents("$datafolder/$fname");
+            $app['content'] = $content;
+
             $block_plugin = $this->plugins->get_registered_type('blocktype', $this->content_type);
             if ($block_plugin && method_exists($block_plugin, 'render')) {
                 $retval .= $block_plugin->render($app, $blockoptions);
